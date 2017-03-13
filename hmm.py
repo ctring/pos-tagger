437da641b7331de83d7482cbaa27de1194892cb0
Cuong Nguyen <nguyentricuong1995@gmail.com>
2017-03-13 02:47:20 -0400

    Added Viterbi algorithm to HMM

--- hmm.py
+++ hmm.py
@@ -16,19 +16,19 @@ class HMM():
         assert i >= 0, 'State "{}" is not defined'.format(state)
         return i
 
-    def update_initial(self, init):
+    def set_initial(self, init):
         init = np.array(init)
         assert init.shape == (self._N,), 'Shape must be {}'.format((self._N,))
         assert np.isclose(np.sum(init), 1.0), 'Sum of all probabilities must be 1.0'
         self._initial = np.copy(init)
 
-    def update_transition(self, from_state, to_state, prob):
+    def set_transition(self, from_state, to_state, prob):
         i_from = self._get_state_index(from_state)
         i_to = self._get_state_index(to_state)
         assert 0.0 <= prob <= 1.0, 'Probability value must lie between 0.0 and 1.0'
         self._transition[i_from][i_to] = prob
 
-    def update_transitions(self, transition_probs):
+    def set_all_transitions(self, transition_probs):
         transition_probs = np.array(transition_probs)
         assert transition_probs.shape == (self._N, self._N), 'Shape must be {}'.format((self._N, self._N))
         row_sum = np.sum(transition_probs, axis=1)
@@ -36,41 +36,42 @@ class HMM():
         assert np.all(0 <= transition_probs) and np.all(transition_probs <= 1.0), 'All values must lie between 0.0 and 1.0'
         self._transition = np.copy(transition_probs)
 
-    def update_emission(self, state, obs, prob):
-        assert 0.0 <= prob <= 1.0, 'Probability value must lie between 0.0 and 1.0'
-        assert state in self._emission, 'State "{}" is not defined'.format(state)
-        self._emission[state][obs] = prob
+    def set_emission(self, state, obs):
+        assert isinstance(obs, dict), 'Emission probabilities must be a dict'
+        self._emission[state] = obs.copy()
 
     def forward(self, obs):
-        acc = np.empty((2, self._N))
-        for s in self._states:
-            i = self._state_index[s]
-            acc[1][i] = self._initial[i] * self._emission[s].get(obs[0], 0)
+        N_obs = len(obs)
+        acc = np.zeros((2, self._N))
+        for j, s in enumerate(self._states):
+            acc[0][j] = self._initial[j] * self._emission[s].get(obs[0], 0)
 
-        for i, o in enumerate(obs[1:]):
+        for i in range(1, N_obs):
             acc[i % 2] = np.dot(acc[1 - i % 2], self._transition)
             for j, s in enumerate(self._states):
-                acc[i % 2][j] *= self._emission[s].get(o, 0)
+                acc[i % 2][j] *= self._emission[s].get(obs[i], 0)
 
-        prob = np.sum(acc[len(obs) % 2])
+        prob = np.sum(acc[(N_obs - 1) % 2])
         return prob
 
-#     def viterbi(self, obs):
-#         v = np.zeros((2, self.N))
-#         bt = np.ones((2, self.N), dtype=np.int)
-#         v[0] = self.pS * self.B[:, obs[0]]
-#         for i, o in enumerate(obs[1:]):
-#             cur = 1 - i % 2
-#             prev = i % 2
-#             for s, vp in enumerate(v[prev]):
-#                 v[cur, s] = np.max(v[prev] * self.A[:, s]) * self.B[s, o]
-#                 bt[cur, s] = np.argmax(v[prev] * self.A[:, s])
-#         prob = np.max(v[1 - len(obs) % 2])
-#         states = np.zeros(self.N)
-#         s = np.argmax(v[1 - len(obs) % 2])
-#         for i in range(len(obs) - 1, -1, -1):
-#             states[i] = s
-#             s = bt[i, s]
-#         return prob, states
+    def viterbi(self, obs):
+        N_obs = len(obs)
+        acc = np.zeros((2, self._N))
+        trace = np.zeros((N_obs, self._N), dtype=np.int)
+        for j, s in enumerate(self._states):
+            acc[0][j] = self._initial[j] * self._emission[s].get(obs[0], 0)
 
+        for i in range(1, N_obs):
+            for j, s in enumerate(self._states):
+                pred = acc[1 - i % 2] * self._transition[:, j]
+                acc[i % 2, j] = np.max(pred) * self._emission[s].get(obs[i], 0)
+                trace[i, j] = np.argmax(pred)
+
+        prob = np.max(acc[(N_obs - 1) % 2])
+        s = np.argmax(acc[(N_obs - 1) % 2])
+        states = []
+        for i in range(N_obs - 1, -1, -1):
+            states.append(self._states[s])
+            s = trace[i, s]
+        return prob, states[::-1]
 
--- hmm_test.py
+++ hmm_test.py
@@ -7,49 +7,62 @@ class TestHMM(unittest.TestCase):
 
   def setUp(self):
     self.model = HMM(['Rain', 'Sun'])
-    self.model.update_initial([0.5, 0.5])
-    self.model.update_transitions([[0.3, 0.7], [0.1, 0.9]])
-    self.model.update_emission('Rain', 'Umbrella', 0.4)
-    self.model.update_emission('Rain', 'Coat', 0.6)
-    self.model.update_emission('Sun', 'Umbrella', 0.3)
-    self.model.update_emission('Sun', 'Sunglasses', 0.7)
+    self.model.set_initial([0.5, 0.5])
+    self.model.set_all_transitions([[0.3, 0.7], [0.1, 0.9]])
+    self.model.set_emission('Rain', {'Umbrella': 0.4, 'Coat': 0.6})
+    self.model.set_emission('Sun', {'Umbrella': 0.3, 'Sunglasses': 0.7})
 
-  def test_update_transition(self):
-    self.model.update_transition('Rain', 'Sun', 0.5)
+  def test_set_transition(self):
+    self.model.set_transition('Rain', 'Sun', 0.5)
     iA = self.model._get_state_index('Rain')
     iB = self.model._get_state_index('Sun')
     self.assertEqual(self.model._transition[iA][iB], 0.5)
 
-  def test_update_transition_invalid_prob(self):
-    self.assertRaises(AssertionError, self.model.update_transition, 'Rain', 'Sun', 2.0)
+  def test_set_transition_invalid_prob(self):
+    self.assertRaises(AssertionError, self.model.set_transition, 'Rain', 'Sun', 2.0)
 
-  def test_update_transition_invalid_state(self):
-    self.assertRaises(AssertionError, self.model.update_transition, 'Snow', 'Sun', 0.1)
+  def test_set_transition_invalid_state(self):
+    self.assertRaises(AssertionError, self.model.set_transition, 'Snow', 'Sun', 0.1)
 
-  def test_update_transitions(self):
+  def test_set_all_transitions(self):
     probs = [[0.2, 0.8], [0.3, 0.7]]
-    self.model.update_transitions(probs)
+    self.model.set_all_transitions(probs)
     self.assertTrue(np.all(self.model._transition == probs))
 
-  def test_update_transitions_invalid_shape(self):
+  def test_set_all_transitions_invalid_shape(self):
     probs = np.array([[0.2, 0.8], [0.3, 0.7], [1.0, 1.0]])
-    self.assertRaises(AssertionError, self.model.update_transitions, probs)
+    self.assertRaises(AssertionError, self.model.set_all_transitions, probs)
 
-  def test_update_emission(self):
-    self.model.update_emission('Rain', 'Umbrella', 0.9)
+  def test_set_emission(self):
+    self.model.set_emission('Rain', {'Umbrella': 0.9})
     self.assertEqual(self.model._emission['Rain']['Umbrella'], 0.9)
 
-  def test_update_initial(self):
-    self.model.update_initial([0.1, 0.9])
+  def test_set_emission_invalid(self):
+    self.assertRaises(AssertionError, self.model.set_emission, 'Rain', [0.9])
+
+  def test_set_initial(self):
+    self.model.set_initial([0.1, 0.9])
     self.assertTrue(np.all(self.model._initial == [0.1, 0.9]))
 
-  def test_update_initial_invalid_shape(self):
-    self.assertRaises(AssertionError, self.model.update_initial, [0.1, 0.2, 0.7])
+  def test_set_initial_invalid_shape(self):
+    self.assertRaises(AssertionError, self.model.set_initial, [0.1, 0.2, 0.7])
 
   def test_forward(self):
     obs = ['Umbrella', 'Coat', 'Sunglasses']
     prob = self.model.forward(obs)
     self.assertAlmostEqual(prob, 0.02205)
+
+  def test_viterbi(self):
+    obs = ['Umbrella', 'Coat', 'Sunglasses']
+    prob, states = self.model.viterbi(obs)
+    self.assertAlmostEqual(prob, 0.01764)
+    self.assertSequenceEqual(states, ['Rain', 'Rain', 'Sun'])
+
+  def test_viterbi_2(self):
+    obs = ['Umbrella', 'Umbrella', 'Umbrella']
+    prob, states = self.model.viterbi(obs)
+    self.assertAlmostEqual(prob, 0.01134)
+    self.assertSequenceEqual(states, ['Rain', 'Sun', 'Sun'])
 
 
 if __name__=='__main__':
